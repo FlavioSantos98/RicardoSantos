a24e1f6429386575ce8d459f11dce56490f1d21b
Flávio Santos <104743306+FlavioSantos98@users.noreply.github.com>
2024-11-08 14:23:12 -0300

    Delete pages/config.php

--- pages/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-    $dbHost = 'LocalHost';
-    $dbUsername = 'root';
-    $dbPassword = '';
-    $dbName = 'mensagemdb';
-
-    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, 3030);
-?>
